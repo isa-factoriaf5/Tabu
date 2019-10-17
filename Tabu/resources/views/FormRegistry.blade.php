9a7ee9376f14c80721b744d9612be3a94c55facb
Jason Carvajal Perez <jasonbnick@gmail.com>
2019-10-17 15:05:30 +0200

    form template

--- Tabu/resources/views/FormRegistry.blade.php
+++ Tabu/resources/views/FormRegistry.blade.php
@@ -1,12 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.master')
+ 
+@section('content')
+ 
+    <h2>Register</h2>
+    <form method="POST" action="/register">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" class="form-control" id="name" name="name">
+        </div>
+ 
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" class="form-control" id="email" name="email">
+        </div>
+ 
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+ 
+        <div class="form-group">
+            <button style="cursor:pointer" type="submit" class="btn btn-primary">Submit</button>
+        </div>
+        @include('partials.formerrors')
+    </form>
+ 
+@endsection
